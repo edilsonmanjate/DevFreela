977ac4f9377399638bab5ebc03cd331c044f30f3
Edilson Manjate <edilsonsh.manjate@gmail.com>
2024-07-16 18:43:07 +0200

    Core first commit

---
+++ DevFreela.Core/Exceptions/ProjectAlreadStartedExeption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Exceptions
+{
+    public class ProjectAlreadStartedExeption : Exception
+    {
+        public ProjectAlreadStartedExeption() : base("Project alread started status")
+        {
+
+        }
+    }
+}
